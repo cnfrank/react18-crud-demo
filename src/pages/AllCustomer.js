2637973fc4e9d51784472dd75759600e7297607a
cnfrank <9570075@qq.com>
2024-03-02 12:07:35 +1300

    hero assets imported

--- src/pages/AllCustomer.js
+++ src/pages/AllCustomer.js
@@ -10,7 +10,7 @@ function AllCustomer() {
       heroAlias: "Sarah Sparks",
       powers:
         "Photonix can manipulate light energy, enabling her to fly at incredible speeds, generate blinding flashes of light, create solid constructs out of photons, and even turn invisible by bending light around herself.",
-      imageUrl: "Photonix.jpg",
+      imageUrl: "Photonix.png",
     },
     {
       id: 2,
@@ -18,7 +18,7 @@ function AllCustomer() {
       heroAlias: "David Steele",
       powers:
         "Titan possesses superhuman strength, durability, and agility. He can also manipulate earth and rock, shaping them into powerful weapons or creating seismic disturbances.",
-      imageUrl: "Titan.jpg",
+      imageUrl: "Titan.png",
     },
     {
       id: 3,
@@ -26,7 +26,7 @@ function AllCustomer() {
       heroAlias: "Emily Winter",
       powers:
         "Frostbite has cryokinetic abilities, allowing her to generate and control ice and cold temperatures. She can freeze objects, create icy constructs, and lower the temperature in her vicinity to freezing levels.",
-      imageUrl: "Frostbite.jpg",
+      imageUrl: "Frostbite.png",
     },
     {
       id: 4,
@@ -34,7 +34,7 @@ function AllCustomer() {
       heroAlias: "Alex Nightshade",
       powers:
         "Shadowstrike possesses the ability to manipulate shadows, using them to teleport short distances, blend into darkness, and create solid constructs formed from shadows. He's also skilled in hand-to-hand combat and stealth.",
-      imageUrl: "Shadowstrike.jpg",
+      imageUrl: "Shadowstrike.png",
     },
     {
       id: 5,
@@ -42,18 +42,18 @@ function AllCustomer() {
       heroAlias: "Marcus Inferno",
       powers:
         "Blaze can generate and control fire. He can project flames from his hands, create walls of fire, and even engulf himself in flames for increased strength and heat resistance. He's also immune to burns and extreme temperatures.",
-      imageUrl: "Blaze.jpg",
+      imageUrl: "Blaze.png",
     },
   ];
 
   return (
     <>
-      <Row xs={1} md={2} className="g-4">
+      <Row xs={1} md={4} className="g-4">
         {heroes.map((hero) => {
           return (
             <Col key={hero.id}>
               <Card>
-                <Card.Img variant="top" src={hero.imageUrl} />
+                <Card.Img variant="top" src={"/assets/hero/"+hero.imageUrl} />
                 <Card.Body>
                   <Card.Title>{hero.heroName}</Card.Title>
                   <Card.Text>{hero.powers}</Card.Text>
